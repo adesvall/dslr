863a732c34b1cde7772dc599389952aabb6e1beb
Augustin DESVALLEES <adesvall@e2r9p2.clusters.42paris.fr>
2023-05-23 16:16:27 +0200

    added ndarray and grad/dbiais

--- logreg_train.py
+++ logreg_train.py
@@ -1,11 +1,7 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import math
-
-df = pd.read_csv("datasets/dataset_train.csv")
-
-df_train = df.sample(frac=0.5, random_state=0)
-df_test = df.drop(df_train.index)
 
 houses = [
     "Slytherin",
@@ -30,7 +26,19 @@ features = [
     "Flying",
 ]
 # theta = [b, poids1, poids2, ...]
-# on va faire 4 theta, un pour chaque maison
+# on va faire 4 theta, un pour chaque maison (one vs all)
+
+######################################################################
+df = pd.read_csv("datasets/dataset_train.csv")
+
+# normalize data
+for column in features:
+    df[column] = (df[column] - df[column].mean()) / df[column].std()
+
+
+df_train = df.sample(frac=0.5, random_state=0)
+df_test = df.drop(df_train.index)
+
 
 def sigmoid(x):
     try:
@@ -50,58 +58,64 @@ def loss(theta, house):
     sum = 0
     for student in df.iloc:
         y = (student["Hogwarts House"] == house)
-        sum += y * math.log(predict(theta, student))
-        sum += (1 - y) * math.log(1 - predict(theta, student))
+        sum -= y * math.log(predict(theta, student))
+        sum -= (1 - y) * math.log(1 - predict(theta, student))
     return sum / len(df)
 
+
+# pred(stud) = sigm(b + sum(theta[i] * stud[feature[i]]))
+# y(stud) = (stud[house] == HOUSE)
+# logloss = sum(     -y * log( pred(stud) ) - (1 - y) * log(1 - pred(stud))       for stud in df)
+# dpred / dtheta[i] = stud[i] * pred() * (1 - pred)
+# grad[i] = dlogloss / dtheta[i] = sum(   -y * stud[i] *( 1 - pred(stud)) - (1 - y) * -stud[i] * pred    for stud in df)
+#                                = sum(   stud[i] * ( y * pred[stud] - y + pred[stud] - y * pred)    for stud in df)
+#                                = sum(   stud[i] * (pred[stud] - y)    for stud in df)
+# dpred / db = pred * (1 - pred)
+# dlogloss / db = = sum(   (pred[stud] - y)    for stud in df)
+
+
 def gradient(theta, house):
-    grad = [0] * (len(features) + 1)
-    for student in df.iloc:
+    grad = np.ndarray(len(features) + 1)
+    for student in df_train.iloc:
         y = (student["Hogwarts House"] == house)
+        grad[0] += (predict(theta, student) - y)
         for i in range(len(features)):
             if not pd.isna(student[features[i]]):
                 grad[i + 1] += (predict(theta, student) - y) * student[features[i]]
-    for i in range(len(grad)):
-        grad[i] /= len(df)
-    return grad
-
-def gradient_descent(theta, house, alpha):
-    grad = gradient(theta, house)
-    for i in range(len(theta)):
-        theta[i] -= alpha * grad[i]
-    return theta
+    return grad / len(df_train)
 
 def train(house):
-    theta = [0] + [0] * len(features)
-    for i in range(30):
-        theta = gradient_descent(theta, house, 0.00001)
-        if i % 3 == 0:
-            print("iter :", i, "accuracy :", onevsall_accuracy(theta, house), end='\r')
+    theta = np.ndarray(len(features) + 1)
+    for i in range(20):
+        print("iter :", i, "accuracy :", onevsall_accuracy(theta, house, df_test), end='\r')
+        theta -= 0.00001 * gradient(theta, house)
     return theta
 
-def onevsall_accuracy(theta, house):
+def onevsall_accuracy(theta, house, data):
     correct = 0
-    for student in df.iloc:
+    for student in data.iloc:
         if (predict(theta, student) > 0.5) == (student["Hogwarts House"] == house):
             correct += 1
-    return correct / len(df)
+    return correct / len(data)
 
 thetas = {}
 for house in houses:
     thetas[house] = train(house)
-    print(house, "accuracy :", onevsall_accuracy(thetas[house], house))
+    print(house, "trainset accuracy :", onevsall_accuracy(thetas[house], house, df_train))
+    print(house, "testset accuracy :", onevsall_accuracy(thetas[house], house, df_test))
 
-def accuracy(thetas):
+def accuracy(thetas, data):
     correct = 0
-    for student in df.iloc:
+    for student in data.iloc:
         max = 0
         for house in houses:
-            if predict(thetas[house], student) > max:
+            if predict(thetas[house], student) >= max:
                 max = predict(thetas[house], student)
                 best = house
         if best == student["Hogwarts House"]:
             correct += 1
-    return correct / len(df)
+    return correct / len(data)
 
-print("accuracy :", accuracy(thetas))
+print("trainset accuracy :", accuracy(thetas, df_train))
+print("testset accuracy :", accuracy(thetas, df_test))
 
